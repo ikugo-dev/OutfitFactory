723f320555248e385a72bee852d9e207d53c1b56
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-08-15 23:38:06 +0200

    deleted the example code

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>We did it, joe!</h1>
   </main>
 </template>

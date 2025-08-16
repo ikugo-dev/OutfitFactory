163835dd0d1f22b44d5f2813e74720f714e34378
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-08-16 13:58:06 +0200

    explore page prototype

---
+++ frontend/src/views/ExploreView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="explore-page">
+    <h2>Explore</h2>
+
+    <select v-model="selectedAlgo" @change="getMorePosts()">
+      <option value="trending">Trending</option>
+      <option value="newest">Newest</option>
+      <option value="forYou">For You</option>
+    </select>
+
+    <PostGrid :posts="posts" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, shallowRef } from 'vue'
+import PostGrid from '../components/PostGrid.vue'
+import { getFakePosts } from '@/fakeData';
+import type { PostType } from '@/types';
+
+const selectedAlgo = ref('trending')
+
+const posts = shallowRef<PostType[]>([])
+
+const getMorePosts = () => posts.value = getFakePosts(5);
+posts.value = getFakePosts(4);
+
+</script>

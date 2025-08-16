760213e55ae414b3181c77e24f97c4b2bc1c1a41
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-08-16 14:43:54 +0200

    added infinite loading on the explore page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.ts";
 
+import InfiniteLoading from "v3-infinite-loading";
+import "v3-infinite-loading/lib/style.css";
+
 const app = createApp(App);
+app.component("infinite-loading", InfiniteLoading);
 app.use(router);
 app.mount("#app");
--- frontend/src/views/ExploreView.vue
+++ frontend/src/views/ExploreView.vue
@@ -9,18 +9,32 @@
     </select>
 
     <PostGrid :posts="posts" />
+
+    <InfiniteLoading @infinite="load" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, shallowRef } from 'vue'
+import InfiniteLoading from "v3-infinite-loading";
+import "v3-infinite-loading/lib/style.css";
+import { ref } from 'vue'
 import PostGrid from '../components/PostGrid.vue'
 import { getFakePosts } from '@/fakeData';
 import type { PostType } from '@/types';
 
-const selectedAlgo = ref('trending')
+const posts = ref<PostType[]>([]);
+posts.value = getFakePosts(2);
+const load = async ($state: { complete(): void, loaded(): void }) => {
+  const response = getFakePosts(4)
+  if (response.length === 0) {
+    $state.complete();
+  } else {
+    posts.value.push(...response);
+    $state.loaded();
+  }
+};
 
-const posts = shallowRef<PostType[]>([])
+const selectedAlgo = ref('trending')
 
 const getMorePosts = () => posts.value = getFakePosts(5);
 posts.value = getFakePosts(4);

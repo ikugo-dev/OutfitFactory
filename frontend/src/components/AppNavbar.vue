5dc37e8d61b9b7d8b46c5ecca9c3fd2fd8a0f38e
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-08-17 16:48:08 +0200

    some navbar and general styling

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -1,19 +1,26 @@
 <template>
   <nav class="navbar">
-    <router-link to="/explore">Explore</router-link>
-    <router-link to="/profile">Profile</router-link>
+    <router-link to="/explore" :class="{ 'active-link': route.path === '/explore' }">Explore</router-link>
+    <router-link to="/profile" :class="{ 'active-link': route.path === '/profile' }">Profile</router-link>
     <!-- <a href="/create.html">Create</a> > -->
   </nav>
 </template>
+
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute();
+</script>
 
 <style scoped>
 .navbar {
   display: flex;
   flex-grow: 1;
   justify-content: space-around;
-  background: teal;
-  border-bottom: 5px solid black;
-  padding: 0.5rem 0;
+  background: var(--background-focus);
+  border-bottom: 4px solid black;
+  padding: 1rem 0;
+  margin-bottom: 5rem;
   top: 0;
   width: 100%;
   left: 0;
@@ -22,11 +29,22 @@
 
 .navbar a {
   text-decoration: none;
-  color: white;
+  color: var(--text);
   font-weight: 900;
 }
 
-.navbar a:hover {
-  background-color: black;
+.navbar a.active-link {
+  color: var(--accent);
 }
 </style>
+
+<!-- .navbar a { -->
+<!--   box-shadow: 6px 6px; -->
+<!--   text-decoration: none; -->
+<!--   background-color: var(--primary); -->
+<!--   padding: 0.5em; -->
+<!-- } -->
+<!---->
+<!-- .navbar a:hover { -->
+<!--   box-shadow: 0px 0px; -->
+<!-- } -->
